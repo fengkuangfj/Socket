d8d0107eab25bbb50d43aa2ec27b6da8d81c8cba
fengkuangfj <fly464447966@126.com>
2015-12-22 08:42:59 +0800

    添加代码

---
+++ PingIP/PingIP.cpp
@@ -0,0 +1,142 @@
+// PingIP.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+BOOL
+	PingIP()
+{
+	BOOL			bRet			= FALSE;
+
+	TCHAR			tchReslt[1000]	= {0};
+	CMD_RESULT_INFO CmdResultInfo	= {0};
+	SYSTEMTIME		SystemTime		= {0};
+
+	CPrintfEx		PrintfEx;
+
+
+	__try
+	{
+		PrintfEx.Init();
+
+		CmdResultInfo.lpResult = tchReslt;
+		CmdResultInfo.ulResultBufferSizeCh = _countof(tchReslt);
+
+		if (!CCommandLine::Execute(
+			_T("ping 10.0.3.254 -n 1"),
+			TRUE,
+			FALSE,
+			&CmdResultInfo
+			))
+		{
+			MessageBox(NULL, _T("CCommandLine::Execute failed"), _T("error"), MB_OK | MB_ICONERROR);
+			__leave;
+		}
+
+		if (!CmdResultInfo.bResult)
+		{
+			GetLocalTime(&SystemTime);
+
+			MessageBox(NULL, _T("CmdResultInfo.bResult failed"), _T("error"), MB_OK | MB_ICONERROR);
+			__leave;
+		}
+
+		printf("%S", CmdResultInfo.lpResult);
+
+		bRet = TRUE;
+	}
+	__finally
+	{
+		;
+	}
+
+	return bRet;
+}
+
+BOOL
+	SocketConnect(
+	__in ULONG ulPort,
+	__in LPSTR lpIP
+	)
+{
+	BOOL		bRet			= FALSE;
+
+	int			iResult			= NO_ERROR;
+	WSADATA		wsaData			= {0};
+	SOCKET		ConnectSocket	= INVALID_SOCKET;
+	sockaddr_in clientService	= {0};
+	SYSTEMTIME	SystemTime		= {0};
+
+
+	__try
+	{
+		do 
+		{
+			//----------------------
+			// Initialize Winsock
+			iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+			if (NO_ERROR != iResult)
+				__leave;
+
+			//----------------------
+			// Create a SOCKET for connecting to server
+			ConnectSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+			if (INVALID_SOCKET == ConnectSocket) 
+				__leave;
+
+			//----------------------
+			// The sockaddr_in structure specifies the address family,
+			// IP address, and port of the server to be connected to.
+			clientService.sin_family = AF_INET;
+			clientService.sin_addr.s_addr = inet_addr(lpIP);
+			clientService.sin_port = htons(ulPort);
+
+			//----------------------
+			// Connect to server.
+			iResult = connect(ConnectSocket, (SOCKADDR *) & clientService, sizeof (clientService));
+			if (SOCKET_ERROR == iResult) 
+			{
+				GetLocalTime(&SystemTime);
+
+				MessageBox(NULL, _T("connect failed"), _T("error"), MB_OK | MB_ICONERROR);
+
+				__leave;
+			}
+
+			if (INVALID_SOCKET != ConnectSocket)
+			{
+				closesocket(ConnectSocket);
+				ConnectSocket = INVALID_SOCKET;
+			}
+
+			WSACleanup();
+
+			Sleep(1000);
+		} while (TRUE);
+
+		bRet = TRUE;
+	}
+	__finally
+	{
+		if (INVALID_SOCKET != ConnectSocket)
+		{
+			closesocket(ConnectSocket);
+			ConnectSocket = INVALID_SOCKET;
+		}
+
+		WSACleanup();
+	}
+
+	return bRet;
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	// PingIP();
+
+	SocketConnect(80, "10.0.3.171");
+
+	_getch();
+
+	return 0;
+}
---
+++ PingIP/stdafx.h
@@ -0,0 +1,25 @@
+// stdafx.h : 标准系统包含文件的包含文件，
+// 或是经常使用但不常更改的
+// 特定于项目的包含文件
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+// TODO: 在此处引用程序需要的其他头文件
+#include <conio.h>
+#include <iostream>
+#include <winsock2.h>
+
+#include "..\\..\\Public\\ProcessType\\ProcessType.h"
+#include "..\\..\\Public\\PrintfEx\\PrintfEx.h"
+#include "..\\..\\Public\\StringInternal\\StringInternal.h"
+#include "..\\..\\Public\\CommandLine\\CommandLine.h"
+
+#pragma comment(lib, "User32.lib")
+#pragma comment(lib, "Kernel32.lib")
+#pragma comment(lib, "ws2_32.lib")
